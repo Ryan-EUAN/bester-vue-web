0849c4a651d9a51a68944beedda1f1a9515be32c
EUAN <euan0721@163.com>
2025-03-13 00:37:14 +0800

    修改端口为服务器端口

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        // target: 'http://localhost:8888',
+        target: 'http://123.60.24.41:8888',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
