dc3de0aa510ee779baad5d6ee49daa57d6f4435a
EUAN <euan0721@163.com>
2025-03-14 19:16:58 +0800

    再次尝试添加CORS

---
+++ functions/web/get_head_info.js
@@ -0,0 +1,17 @@
+export async function onRequest(context) {
+    const { request } = context;
+    const response = await context.next();
+
+    // 设置 CORS 头
+    response.headers.set("Access-Control-Allow-Origin", "http://123.60.24.41:8888");
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    response.headers.set("Access-Control-Allow-Credentials", "true");
+
+    // 处理 OPTIONS 预检请求
+    if (request.method === "OPTIONS") {
+        return new Response(null, { headers: response.headers });
+    }
+
+    return response;
+}
