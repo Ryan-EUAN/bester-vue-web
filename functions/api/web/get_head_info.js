4cae2fbf9aa551459b6fe4b6379a139b90dbf9c4
EUAN <euan0721@163.com>
2025-03-15 09:44:43 +0800

    Debug

--- functions/api/web/get_head_info.js
+++ functions/api/web/get_head_info.js
@@ -1,19 +1,20 @@
 export async function onRequest(context) {
-    console.log(`请求地址${context.params.id}`);
+    return new Response(`请求地址${context.params.id}`);
+    // console.log(`请求地址${context.params.id}`);
 
-    const { request } = context;
-    const response = await context.next();
+    // const { request } = context;
+    // const response = await context.next();
 
-    // 设置 CORS 头
-    response.headers.set("Access-Control-Allow-Origin", "http://123.60.24.41:8888");
-    response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    response.headers.set("Access-Control-Allow-Credentials", "true");
+    // // 设置 CORS 头
+    // response.headers.set("Access-Control-Allow-Origin", "http://123.60.24.41:8888");
+    // response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    // response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    // response.headers.set("Access-Control-Allow-Credentials", "true");
 
-    // 处理 OPTIONS 预检请求
-    if (request.method === "OPTIONS") {
-        return new Response(null, { headers: response.headers });
-    }
+    // // 处理 OPTIONS 预检请求
+    // if (request.method === "OPTIONS") {
+    //     return new Response(null, { headers: response.headers });
+    // }
 
-    return response;
+    // return response;
 }
