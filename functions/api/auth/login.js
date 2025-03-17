69f8797c8f949cc71f9378313d81611feac44ac2
EUAN <euan0721@163.com>
2025-03-17 12:48:35 +0800

    修改一下登录CORS

--- functions/api/auth/login.js
+++ functions/api/auth/login.js
@@ -1,6 +1,6 @@
 export async function onRequest({ request }) {
     const body = await request.json();
-    const res = await fetch('http://123.60.24.41/api/user/login', {
+    const res = await fetch('http://123.60.24.41/api/auth/user/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json; charset=UTF-8',
