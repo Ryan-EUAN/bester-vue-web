bce23f7199f81ed395fee8ad31836c49816a0320
EUAN <euan0721@163.com>
2025-03-18 19:00:44 +0800

    修复问题

--- functions/api/auth/user/check-token.js
+++ functions/api/auth/user/check-token.js
@@ -1,16 +1,5 @@
 export async function onRequest({ request }) {
-    const body = await request.json();
-    const method = request.method;
     const token = request.headers.get('Authorization');
-    // const res = await fetch('http://123.60.24.41/api/auth/user/check-token', {
-    //     method: method,
-    //     headers: {
-    //         // 'Content-Type': 'application/json; charset=UTF-8',
-    //         // 'Access-Control-Allow-Origin': '*',
-    //         'Authorization': token
-    //     },
-    //     body: JSON.stringify(body)
-    // })
     const res = await fetch('http://123.60.24.41/api/auth/user/check-token', {
         method: request.method,
         headers: {
@@ -18,9 +7,7 @@ export async function onRequest({ request }) {
         },
         body: request.body,
     });
-
-    const data = await res.json();
-    return new Response(JSON.stringify(data), {
+    return new Response(res.body, {
         headers: {
             'content-type': 'application/json; charset=UTF-8',
             'Access-Control-Allow-Origin': '*',
--- functions/api/user/profile.js
+++ functions/api/user/profile.js
@@ -1,26 +1,5 @@
-// export async function onRequest({ request }) {
-//     const body = await request.json();
-//     const method = request.method;
-//     const token = request.headers.get('Authorization');
-
-//     const res = await fetch('http://123.60.24.41/api/user/profile', {
-//         method: request.method,
-//         headers: {
-//             Authorization: token,
-//         },
-//         body: request.body,
-//     });
-//     const data = await res.json();
-//     return new Response(JSON.stringify(data), {
-//         headers: {
-//             'content-type': 'application/json; charset=UTF-8',
-//             'Access-Control-Allow-Origin': '*',
-//         },
-//     });
-// }
 export async function onRequest({ request }) {
     const token = request.headers.get('Authorization');
-    console.log(token);
     const res = await fetch('http://123.60.24.41/api/user/profile', {
         method: request.method,
         headers: {
