66fa9059f8c97a0a55a2f5da85634bbe22e933a6
EUAN <euan0721@163.com>
2025-03-18 19:12:26 +0800

    同一个接口，两种请求方式，测试一下

--- functions/api/user/profile.js
+++ functions/api/user/profile.js
@@ -1,16 +1,34 @@
 export async function onRequest({ request }) {
     const token = request.headers.get('Authorization');
-    const res = await fetch('http://123.60.24.41/api/user/profile', {
-        method: request.method,
-        headers: {
-            Authorization: token,
-        },
-        body: request.body,
-    });
-    return new Response(res.body, {
-        headers: {
-            'content-type': 'application/json; charset=UTF-8',
-            'Access-Control-Allow-Origin': '*',
-        },
-    });
+    if (request.method == 'GET') {
+        const res = await fetch('http://123.60.24.41/api/user/profile', {
+            method: request.method,
+            headers: {
+                Authorization: token,
+            },
+            body: request.body,
+        });
+        return new Response(res.body, {
+            headers: {
+                'content-type': 'application/json; charset=UTF-8',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    }
+    if (request.method == 'PUT') {
+        const body = await request.json();
+        const res = await fetch('http://123.60.24.41/api/user/profile', {
+            method: request.method,
+            headers: {
+                Authorization: token,
+            },
+            body: JSON.stringify(body)
+        });
+        return new Response(res.body, {
+            headers: {
+                'content-type': 'application/json; charset=UTF-8',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    }
 }
