8020340881e86ae83b3056b25b235165205587fe
EUAN <euan0721@163.com>
2025-03-18 18:57:00 +0800

    再次尝试

--- functions/api/user/profile.js
+++ functions/api/user/profile.js
@@ -1,8 +1,26 @@
-export async function onRequest({ request }) {
-    const body = await request.json();
-    const method = request.method;
-    const token = request.headers.get('Authorization');
+// export async function onRequest({ request }) {
+//     const body = await request.json();
+//     const method = request.method;
+//     const token = request.headers.get('Authorization');
 
+//     const res = await fetch('http://123.60.24.41/api/user/profile', {
+//         method: request.method,
+//         headers: {
+//             Authorization: token,
+//         },
+//         body: request.body,
+//     });
+//     const data = await res.json();
+//     return new Response(JSON.stringify(data), {
+//         headers: {
+//             'content-type': 'application/json; charset=UTF-8',
+//             'Access-Control-Allow-Origin': '*',
+//         },
+//     });
+// }
+export async function onRequest({ request }) {
+    const token = request.headers.get('Authorization');
+    console.log(token);
     const res = await fetch('http://123.60.24.41/api/user/profile', {
         method: request.method,
         headers: {
@@ -10,8 +28,7 @@ export async function onRequest({ request }) {
         },
         body: request.body,
     });
-    const data = await res.json();
-    return new Response(JSON.stringify(data), {
+    return new Response(res.body, {
         headers: {
             'content-type': 'application/json; charset=UTF-8',
             'Access-Control-Allow-Origin': '*',
