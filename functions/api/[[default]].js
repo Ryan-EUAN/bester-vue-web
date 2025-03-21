7ff3c22f79be2d43fc6c3cdc3a53ead6ad1e5e25
EUAN <euan0721@163.com>
2025-03-21 21:48:47 +0800

    修改解析CORS

--- functions/api/[[default]].js
+++ functions/api/[[default]].js
@@ -2,11 +2,13 @@ export async function onRequest({ request }) {
     const reqHeaders = new Headers(request.headers);
     reqHeaders.delete('host');
     const url = new URL(request.url);
-    const res = await fetch(`http://123.60.24.41${url.pathname}`, {
+    // console.log('查看日志', url.search);
+    const res = await fetch(`http://123.60.24.41${url.pathname}${url.search}`, {
         method: request.method,
         headers: reqHeaders,
         body: request.body,
     });
+
 
     const resHeaders = new Headers(res.headers);
     resHeaders.set('Access-Control-Allow-Origin', '*');
